d58f4c563de51cbe8a2a8074e8cc7e78f48ed88f
Muhammad Hassan <hassantariq.8631@gmail.com>
2016-02-26 15:54:49 -0600

    Initial

---
+++ Scripts/app/app.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngMaterial']);
+
+    app.controller('progressController', function (loaderService) {
+
+        var vm = this;
+        
+        vm.showLoader = loaderService.getLoaderValue();
+
+        vm.show = function () {
+            vm.showLoader = loaderService.showLoader();
+        };
+
+        vm.hide = function () {
+            vm.showLoader = loaderService.hideLoader();
+        };
+    });
+
+})();
---
+++ Scripts/app/loaderDirective.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('loader', loader);
+
+    function loader() {
+        var directive = {
+            restrict: 'EA',
+            templateUrl: '/scripts/app/loaderDirective.html',
+        };
+
+        return directive;
+    }
+
+    loaderController.$inject = [];
+
+    function loaderController() {
+
+        var vm = this;
+
+        init();
+
+        /////////////////////Implementation///////////////////////////////
+
+        function init() {
+           
+        }
+    }
+
+})();
